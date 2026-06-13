33512c9ccd67d808496a929c87a8967beb019183
727823tuit139-sudo <727823tuit139>
2026-06-13 22:12:57 +0530

    chore: update web bootstrap service worker version before final launch

--- frontend/bike_taxi_app/build/web/flutter_bootstrap.js
+++ frontend/bike_taxi_app/build/web/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"e4b8dca3f1b4ede4c30371002441c88c12187e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1905409715" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "161992694" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
