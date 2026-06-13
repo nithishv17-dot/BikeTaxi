e96392c444e9569597e18b6aa6943553c101c46c
727823tuit139-sudo <727823tuit139>
2026-06-13 22:21:47 +0530

    chore: push verified flutter web build artifacts for release

--- frontend/bike_taxi_app/build/web/flutter_bootstrap.js
+++ frontend/bike_taxi_app/build/web/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"e4b8dca3f1b4ede4c30371002441c88c12187e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "161992694" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3317778549" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
