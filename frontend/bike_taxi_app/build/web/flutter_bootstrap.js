9e82781d37bcbf277e3e5671ee0c31ff85e8736a
727823tuit139-sudo <727823tuit139>
2026-06-14 23:05:46 +0530

    Rebuild web bundle for home startup

--- frontend/bike_taxi_app/build/web/flutter_bootstrap.js
+++ frontend/bike_taxi_app/build/web/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"e4b8dca3f1b4ede4c30371002441c88c12187e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "220306850" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3778198773" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
